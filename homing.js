249694ecc1829866abea8d86509405481cd29306
Sofia Chalkias <schalkia@umd.edu>
2023-03-25 15:07:10 -0400

    touchups to QOTD and home page style

--- homing.js
+++ homing.js
@@ -4,6 +4,21 @@ window.onload = main;
 // let DayOfWeekArr = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
 function main() {
     let DayOfWeek = 'Monday';
+    let quotesOfDay = ['When we do the best we can, we never know what miracle is wrought in our life or the life of another. – Helen Keller',
+    'When it is obvious that goals can’t be reached, don’t adjust the goals, but adjust the action steps. – Confucius',
+    'The whole secret of a successful life is to find out what is one’s destiny to do, and then do it. – Henry Ford',
+    'The willing, Destiny guides them. The unwilling, Destiny drags them. – Seneca',
+    'Experience is a wonderful thing. It enables you to recognize a mistake when you make it again. – Franklin Jones',
+    'Never let the fear of striking out keep you from playing the game.” – Babe Ruth',
+    'The way I see it, if you want the rainbow, you gotta put up with the rain.” – Dolly Parton',
+    'When we strive to become better than we are, everything around us becomes better too.” – Paulo Coelho',
+    '“Worry never climbed a hill. Worry never paid a bill. Worry never dried a tear. Worry never calmed a fear." – Eleanor H Porter',
+    '“If you want to live a happy life, tie it to a goal, not to people or things.” – Albert Einstein',
+    '“Being cool is when you win, you don’t get too happy; and when you lose, you don’t get too mad.” – Ice T',
+    'Never give up. When your heart becomes tired, just walk with your legs – but move on.” – Paulo Coelho',
+    'Everything negative – pressure, challenges – is all an opportunity for me to rise.” – Kobe Bryant'];
+    num = 0;
+
     // Set interval for the day of the week changing every 20 seconds
    setInterval(function () {
     /* We want to set the correct active fire image to the day of the week that it currently is */
@@ -45,6 +60,14 @@ function main() {
             // Make sure you change the previous image back to the normal one            
             DayOfWeek = 'Monday';
             break;
-    }
-   }, 10000);
+    
+   }
+        // Adjust the quotes of the day each day
+        if (num != quotesOfDay.length - 1) {
+            document.getElementById("quote").innerHTML = quotesOfDay[num];
+            num++;
+        } else {
+            num = 0;
+        }
+    }, 10000);
 }
