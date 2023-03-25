9a430ad952be3e210026a159f42205cd4f931276
Sofia Chalkias <schalkia@umd.edu>
2023-03-25 16:03:50 -0400

    template other stretches implementation

--- stretching.js
+++ stretching.js
@@ -1,4 +1,43 @@
-window.onload = main;
+// When the back image is clicked on, it goes to the appropriate stretches page
+function clickBack() {
+        if(document.getElementById("level").innerHTML === "Beginner") {
+            location.href = 'beginnerBack.html';
+        } else if (document.getElementById("level").innerHTML === "Intermediate") {
+            location.href = 'intermediateBack.html';
+        } else if (document.getElementById("level").innerHTML === "Advanced") {
+            location.href = 'advancedBack.html';
+        }
+}
 
-function main() {
+// When the arms image is clicked on, it goes to the appropriate stretches page
+function clickArms() {
+    if(document.getElementById("level").innerHTML === "Beginner") {
+        location.href = 'beginnerArms.html';
+    } else if (document.getElementById("level").innerHTML === "Intermediate") {
+        location.href = 'intermediateArms.html';
+    } else if (document.getElementById("level").innerHTML === "Advanced") {
+        location.href = 'advancedArms.html';
+    }
+}
+
+// When the abs image is clicked on, it goes to the appropriate stretches page
+function clickAbs() {
+    if(document.getElementById("level").innerHTML === "Beginner") {
+        location.href = 'beginnerAbs.html';
+    } else if (document.getElementById("level").innerHTML === "Intermediate") {
+        location.href = 'intermediateAbs.html';
+    } else if (document.getElementById("level").innerHTML === "Advanced") {
+        location.href = 'advancedAbs.html';
+    }
+}
+
+// When the legs image is clicked on, it goes to the appropriate stretches page
+function clickLegs() {
+    if(document.getElementById("level").innerHTML === "Beginner") {
+        location.href = 'beginnerLegs.html';
+    } else if (document.getElementById("level").innerHTML === "Intermediate") {
+        location.href = 'intermediateLegs.html';
+    } else if (document.getElementById("level").innerHTML === "Advanced") {
+        location.href = 'advancedLegs.html';
+    }
 }
