f24f55f8ea6326dea699581b2680dd99f6a4d117
DanielPark <Mighty-Mango>
2023-03-30 23:58:00 -0400

    add some function to nutrition

---
+++ nutritionScript.js
@@ -0,0 +1,10 @@
+
+    function openForm(){
+        document.getElementById("form").style.display = "block";
+
+    }
+    function closeForm(){
+        document.getElementById("form").style.display = "none";
+    }
+    
+    
