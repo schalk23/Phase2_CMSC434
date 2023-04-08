adb059d969af0f7a3a7b046cbbbfcebff1fe3923
DanielPark <Mighty-Mango>
2023-04-08 13:46:32 -0400

    updating3

--- nutritionScript.js
+++ nutritionScript.js
@@ -1,109 +1,113 @@
+var totalCal = 0;
+var totalFat = 0;
+var totalCarb = 0;
+var totalProtein = 0;
+var totalWaterInTake = 0;
+var eat = false;
 
-    let breakfastbool = false;
-    let lunchbool = false;
-    let dinnerbool = false;
 
-    function clicktrue(){
-        breakfastbool = true;
-        lunchbool = true;
-        dinnerbool = true;
+    function reset(){
+        eat = false;
+        document.getElementById("bfcalories").value = '';
+        document.getElementById("bffat").value = '';
+        document.getElementById("bfcarb").value = '';
+        document.getElementById("bfprotein").value = '';
+        document.getElementById("bfwaterintake").value = '';
     }
-
-    function openForm(){
+    function ifbreakfast(){
+        eat = true;
+        document.getElementById("bf").innerHTML = 'Breakfast';
+    }
+    function iflunch(){
+        eat = true;
+        document.getElementById("bf").innerHTML = 'Lunch';
+    }
+    function ifdinner(){
+        eat = true;
+        document.getElementById("bf").innerHTML = 'Dinner';
+    }
+    function openForm() {
         document.getElementById("form").style.display = "block";
-
     }
-    function writeData(){
-        const fs = require('fs');
-        console.log(breakfastbool);
-        let calories = document.getElementById("caloryintake").value;
-        let sat = document.getElementById("satfatintake").value;
-        let trans = document.getElementById("transfatintake").value;
-        let chonum = document.getElementById("cholesterolinput").value;
-        let sodnum = document.getElementById("sodiuminput").value;
-        let carbnum = document.getElementById("carbinput").value;
-        let fibernum = document.getElementById("fiberinput").value;
-        let tsugarnum = document.getElementById("totalsugarinput").value;
-        let asugarnum = document.getElementById("addedsugarinput").value;
-        let proteinnum = document.getElementById("proteininput").value;
-        let data = "";
-        if(breakfastbool){
-            document.getElementById("totalfatnum").value = "deeznutz"
-            data = "breakfast: calories: "+calories+"saturated fat: "+sat;
-            fs.writeFile('nutritiondata.txt',data,(err)=> {
-                if(err) throw err;
-            })
+      
+      function closeForm() {
+        document.getElementById("form").style.display = "none";
+    }
+
+    function addtoTotal(){
+        
+
+        var bfcal = document.getElementById("bfcalories");
+        var bffat = document.getElementById("bffat");
+        var bfcarb = document.getElementById("bfcarb");
+        var bfprotein = document.getElementById("bfprotein");
+        var bfwater = document.getElementById("bfwaterintake");
+
+        var callabel = document.getElementById("caloriesnum");
+        var fatlabel = document.getElementById("fatnum");
+        var carblabel = document.getElementById("carbnum");
+        var proteinlabel = document.getElementById("proteinnum");
+        var waterlabel = document.getElementById("waternum");
+        var watermeter = document.getElementById("watermeter");
+        var goal = 100;
+
+        var toIntcal = parseInt(bfcal.value);
+        var toIntfat = parseInt(bffat.value);
+        var toIntcarb = parseInt(bfcarb.value);
+        var toIntprotein = parseInt(bfprotein.value);
+        var toIntwater = parseInt(bfwater.value);
+
+        console.log(isNaN(toIntcal));
+
+        if(eat){
+            if(isNaN(toIntcal) == false && isNaN(toIntfat) == false && isNaN(toIntcarb) == false && isNaN(toIntprotein) == false && isNaN(toIntwater) == false){
+                console.log("if");
+                totalCal = parseInt(callabel.innerHTML) + parseInt(bfcal.value);
+                callabel.innerHTML = totalCal;
+
+                totalFat = parseInt(fatlabel.innerHTML) + parseInt(bffat.value);
+                fatlabel.innerHTML = totalFat;
+
+                totalCarb = parseInt(carblabel.innerHTML) + parseInt(bfcarb.value);
+                carblabel.innerHTML = totalCarb;
+
+                totalProtein = parseInt(proteinlabel.innerHTML) + parseInt(bfprotein.value);
+                proteinlabel.innerHTML = totalProtein;
+
+                totalWaterInTake = parseInt(waterlabel.innerHTML) + parseInt(bfwater.value);
+                waterlabel.innerHTML = totalWaterInTake;
+                watermeter.value = totalWaterInTake;
+            }
+            else{
+                console.log("else");
+                if(isNaN(toIntcal)){
+                    totalCal = parseInt(callabel.innerHTML);
+                    callabel.innerHTML = totalCal;
+                }
+                if(isNaN(toIntfat)){
+                    totalFat = parseInt(fatlabel.innerHTML);
+                    fatlabel.innerHTML = totalFat;
+                }
+                if(isNaN(toIntcarb)){
+                    totalCarb = parseInt(carblabel.innerHTML);
+                    carblabel.innerHTML = totalCarb;
+                }
+                if(isNaN(toIntprotein)){
+                    totalProtein = parseInt(proteinlabel.innerHTML);
+                    proteinlabel.innerHTML = totalProtein;
+                }
+                if(isNaN(toIntwater)){
+                    totalWaterInTake = parseInt(waterlabel.innerHTML);
+                    waterlabel.innerHTML = totalWaterInTake;
+                }
+                
+            }
             
         }
-        
-    }
-    function reset(){
-        document.getElementById("caloryintake").value = '';
-        document.getElementById("satfatintake").value = '';
-        document.getElementById("transfatintake").value = '';
-        document.getElementById("cholesterolinput").value = '';
-        document.getElementById("sodiuminput").value = '';
-        document.getElementById("carbinput").value = '';
-        document.getElementById("fiberinput").value = '';
-        document.getElementById("totalsugarinput").value = '';
-        document.getElementById("addedsugarinput").value = '';
-        document.getElementById("proteininput").value = '';
-        breakfastbool = false;
-        lunchbool = false;
-        dinnerbool = false;
-        
-    }
-    function closeForm(){
-        //reset all other values
-        //need to write to textfile
-        
-        document.getElementById("form").style.display = "none";
-        
-    }
-    function digitLimit(){
-        let sat = document.getElementById("satfatintake");
-        let trans = document.getElementById("transfatintake");
-        let chonum = document.getElementById("cholesterolinput");
-        let sodnum = document.getElementById("sodiuminput");
-        let carbnum = document.getElementById("carbinput");
-        let fibernum = document.getElementById("fiberinput");
-        let tsugarnum = document.getElementById("totalsugarinput");
-        let asugarnum = document.getElementById("addedsugarinput");
-        let proteinnum = document.getElementById("proteininput");
 
-        document.getElementById("totalfatnum").innerHTML = "test";
 
-        if (sat.value.length > sat.maxLength){
-            sat.value = sat.value.slice(0, sat.maxLength);
-        }
-        if (trans.value.length > trans.maxLength){
-            trans.value = trans.value.slice(0, trans.maxLength);
-        }
-        if (chonum.value.length > chonum.maxLength){
-            chonum.value = chonum.value.slice(0, chonum.maxLength);
-        }
-        if (sodnum.value.length > sodnum.maxLength){
-            sodnum.value = sodnum.value.slice(0, sodnum.maxLength);
-        }
-        if (carbnum.value.length > carbnum.maxLength){
-            carbnum.value = carbnum.value.slice(0, carbnum.maxLength);
-        }
-        if (fibernum.value.length > fibernum.maxLength){
-            fibernum.value = fibernum.value.slice(0, fibernum.maxLength);
-        }
-        if (tsugarnum.value.length > tsugarnum.maxLength){
-            tsugarnum.value = tsugarnum.value.slice(0, tsugarnum.maxLength);
-        }
-        if (asugarnum.value.length > asugarnum.maxLength){
-            asugarnum.value = asugarnum.value.slice(0, asugarnum.maxLength);
-        }
-        if (proteinnum.value.length > proteinnum.maxLength){
-            proteinnum.value = proteinnum.value.slice(0, proteinnum.maxLength);
-        }
-        
-        // let totalfat = sat.value + trans.value;
-        // fatnum.innerHTML = totalfat;
     }
-    //need to limit the digits
-    //need a function to limit the digit as well as update the labels
-    
+
+
+
+   
