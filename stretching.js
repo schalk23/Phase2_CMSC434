eac518bb25e3b5a2e95828e8284c23c18367673e
Sofia Chalkias <schalkia@umd.edu>
2023-04-09 20:51:20 -0400

    Drop down for levels, sizing, buttons, functions

--- stretching.js
+++ stretching.js
@@ -1,57 +1,48 @@
-let stretchesArr = ["BAR1", "BAR2", "BA1", "BA2", "BA3", "BB1", "BB" ]// When the back image is clicked on, it goes to the appropriate stretches page
-
+localStorage.setItem('stretches', JSON.stringify([]));
+// When the back image is clicked on, it goes to the appropriate stretches page
 function clickBack() {
-        if(document.getElementById("level").innerHTML === "Beginner") {
+        if(document.getElementById("level").value === "Beginner") {
             location.href = 'beginnerBack.html';
-        } else if (document.getElementById("level").innerHTML === "Intermediate") {
+        } else if (document.getElementById("level").value === "Intermediate") {
             location.href = 'intermediateBack.html';
-        } else if (document.getElementById("level").innerHTML === "Advanced") {
+        } else if (document.getElementById("level").value === "Advanced") {
             location.href = 'advancedBack.html';
         }
 }
 
 // When the arms image is clicked on, it goes to the appropriate stretches page
 function clickArms() {
-    if(document.getElementById("level").innerHTML === "Beginner") {
+    if(document.getElementById("level").value === "Beginner") {
         location.href = 'beginnerArms.html';
-    } else if (document.getElementById("level").innerHTML === "Intermediate") {
+    } else if (document.getElementById("level").value === "Intermediate") {
         location.href = 'intermediateArms.html';
-    } else if (document.getElementById("level").innerHTML === "Advanced") {
+    } else if (document.getElementById("level").value === "Advanced") {
         location.href = 'advancedArms.html';
     }
 }
 
 // When the abs image is clicked on, it goes to the appropriate stretches page
 function clickAbs() {
-    if(document.getElementById("level").innerHTML === "Beginner") {
+    if(document.getElementById("level").value === "Beginner") {
         location.href = 'beginnerAbs.html';
-    } else if (document.getElementById("level").innerHTML === "Intermediate") {
+        
+    } else if (document.getElementById("level").value === "Intermediate") {
         location.href = 'intermediateAbs.html';
-    } else if (document.getElementById("level").innerHTML === "Advanced") {
+    } else if (document.getElementById("level").value === "Advanced") {
         location.href = 'advancedAbs.html';
     }
 }
 
 // When the legs image is clicked on, it goes to the appropriate stretches page
 function clickLegs() {
-    if(document.getElementById("level").innerHTML === "Beginner") {
+    if(document.getElementById("level").value === "Beginner") {
         location.href = 'beginnerLegs.html';
-    } else if (document.getElementById("level").innerHTML === "Intermediate") {
+    } else if (document.getElementById("level").value === "Intermediate") {
         location.href = 'intermediateLegs.html';
-    } else if (document.getElementById("level").innerHTML === "Advanced") {
+    } else if (document.getElementById("level").value === "Advanced") {
         location.href = 'advancedLegs.html';
     }
 }
-
-// Passing in an argument here, using the onchange property in the HTML allows us to track if the checkbox is checked
-// function ifChecked(arg) {
-//     if(document.getElementById("level").innerHTML === "Advanced") {
-//        if(document.getElementById('AL1').checked) {
-//         console.log("YES");
-//         } 
-//     }
-    
-//}
 let SOTD1 = `./stretch_images/SOTD${Math.floor(Math.random() * 14) + 1}.jpg`;
 let SOTD2 = `./stretch_images/SOTD${Math.floor(Math.random() * 14) + 1}.jpg`;
 let SOTD3 = `./stretch_images/SOTD${Math.floor(Math.random() * 14) + 1}.jpg`;
@@ -70,44 +61,97 @@ function currentSlide(dotNum) {
 }
 
 // When the user submits their stretches, they see the format of the button change
-function submittedStretches() {
+function submittedStretches(page) {
     document.getElementById("buttonS").innerHTML = "<h2 style=\"background-color:#fffae0ff; color:black;\"> Stretches Submitted! </h2>";
     document.getElementById("buttonS").style["background-color"] = "#fffae0ff";
-    document.getElementById("BA1").checked = false;
-    document.getElementById("BA2").checked = false;
-    document.getElementById("BA3").checked = false;
-    document.getElementById("IA1").checked = false;
-    document.getElementById("IA2").checked = false;
-    document.getElementById("IA3").checked = false;
-    document.getElementById("AA1").checked = false;
-    document.getElementById("AA2").checked = false;
-    document.getElementById("AA3").checked = false;
-    document.getElementById("BB1").checked = false;
-    document.getElementById("BB2").checked = false;
-    document.getElementById("BB3").checked = false;
-    document.getElementById("IB1").checked = false;
-    document.getElementById("IB2").checked = false;
-    document.getElementById("IB3").checked = false;
-    document.getElementById("AB1").checked = false;
-    document.getElementById("AB2").checked = false;
-    document.getElementById("AB3").checked = false;
-    document.getElementById("BAR1").checked = false;
-    document.getElementById("BAR2").checked = false;
-    document.getElementById("BAR3").checked = false;
-    document.getElementById("IAR1").checked = false;
-    document.getElementById("IAR2").checked = false;
-    document.getElementById("IAR3").checked = false;
-    document.getElementById("AAR1").checked = false;
-    document.getElementById("AAR2").checked = false;
-    document.getElementById("AAR3").checked = false;
-    document.getElementById("BL1").checked = false;
-    document.getElementById("BL2").checked = false;
-    document.getElementById("BL3").checked = false;
-    document.getElementById("IL1").checked = false;
-    document.getElementById("IL2").checked = false;
-    document.getElementById("IL3").checked = false;
-    document.getElementById("AL1").checked = false;
-    document.getElementById("AL2").checked = false;
-    document.getElementById("AL3").checked = false;
+    
+    // user must be on the current page in order for this .checked to work
+    //the passed in page so that we can uncheck all of the stretches they submitted
+    if(page === "BA") {
+        const s = JSON.parse(localStorage.getItem('stretches') || '[]');
+        if(document.getElementById("BA1").checked) {
+            s.push("BA1");
+            console.log(s);
+            localStorage.setItem('stretches', JSON.stringify(stretches));
+        }
+        if(document.getElementById("BA2").checked) {
+            stretch.push("BA2");
+            console.log(stretches);
+            localStorage.setItem('stretches', JSON.stringify(stretches));
+        }
+        if(document.getElementById("BA3").checked) {
+            stretch.push("BA3");
+            console.log(stretches);
+            localStorage.setItem('stretches', JSON.stringify(stretches));
+        }
+        document.getElementById("BA1").checked = false;
+        document.getElementById("BA2").checked = false;
+        document.getElementById("BA3").checked = false; 
+    }
+    
+    if(page === "IA") {
+        document.getElementById("IA1").checked = false;
+        document.getElementById("IA2").checked = false;
+        document.getElementById("IA3").checked = false;
+    }
 
+    if(page === "AA") {
+        document.getElementById("AA1").checked = false;
+        document.getElementById("AA2").checked = false;
+        document.getElementById("AA3").checked = false;
+    }
+
+    if(page === "BB") {
+        document.getElementById("BB1").checked = false;
+        document.getElementById("BB2").checked = false;
+        document.getElementById("BB3").checked = false;
+    }
+
+    if(page === "IB") {
+        document.getElementById("IB1").checked = false;
+        document.getElementById("IB2").checked = false;
+        document.getElementById("IB3").checked = false;
+    }
+
+    if(page === "AB") {
+        document.getElementById("AB1").checked = false;
+        document.getElementById("AB2").checked = false;
+        document.getElementById("AB3").checked = false;
+    }
+
+        if(page === "BAR") {
+        document.getElementById("BAR1").checked = false;
+        document.getElementById("BAR2").checked = false;
+        document.getElementById("BAR3").checked = false;
+    }
+
+    if(page === "IAR") {
+        document.getElementById("IAR1").checked = false;
+        document.getElementById("IAR2").checked = false;
+        document.getElementById("IAR3").checked = false;
+    }
+
+    if(page === "AAR") {
+        document.getElementById("AAR1").checked = false;
+        document.getElementById("AAR2").checked = false;
+        document.getElementById("AAR3").checked = false;
+    }
+
+    if(page === "BL") {
+        document.getElementById("BL1").checked = false;
+        document.getElementById("BL2").checked = false;
+        document.getElementById("BL3").checked = false;
+    }
+
+        if(page === "IL") {
+        document.getElementById("IL1").checked = false;
+        document.getElementById("IL2").checked = false;
+        document.getElementById("IL3").checked = false;
+    }
+
+    if(page === "AL") {
+        document.getElementById("AL1").checked = false;
+        document.getElementById("AL2").checked = false;
+        document.getElementById("AL3").checked = false;
+    }
 }
